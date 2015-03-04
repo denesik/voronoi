ee2cdd7fc0a9047e2bfe6c65c4a44b1d4367f3c4
denesik <denesic@gmail.com>
2015-03-04 22:24:02 +0300

    Освобождение ресурсов, рисование.

--- main.cpp
+++ main.cpp
@@ -40,7 +40,7 @@ void Generate(std::vector<glm::vec2> &points, const glm::uvec2 &size)
     glm::vec2 operator()() {return glm::vec2(rand() % size.x, rand() % size.y);}
   } generator(size);
 
-  std::generate_n(std::inserter(p, p.end()), 5000000, generator);
+  std::generate_n(std::inserter(p, p.end()), 1000000, generator);
 
   points.reserve(p.size());
   points.insert(points.end(), p.begin(), p.end());
@@ -52,63 +52,48 @@ int main()
 
     std::vector<glm::vec2> points;
     Generate(points, size);
-    /*
+/*
     points.push_back(glm::vec2(20, 20));
     points.push_back(glm::vec2(40, 60));
     points.push_back(glm::vec2(60, 40));
     points.push_back(glm::vec2(80, 80));
     points.push_back(glm::vec2(20, 80));
-    */
+*/
     printf("%7gs End generate, Count: %i\n", get_msec(), static_cast<int>(points.size()));
 
     printf("%7gs Start Voronoi\n", get_msec());
     Voronoi v(points, size);
     printf("%7gs End Voronoi\n", get_msec());
 
-/*    v.Sort();
-    auto diagram = v.GetDiagram();
+    const std::vector<glm::vec2> &vertex = v.GetVertex();
+    const std::vector<Voronoi::Edge> &edge = v.GetEdges();
+
     printf("%7gms Start drawing\n", get_msec());
 
     Image image;
     image.Resize(size.x + 1, size.y + 1);
     image.Fill(0xFFFFFFFF);
 
-    for(auto it = diagram.begin(); it != diagram.end(); ++it)
+    for(auto it = edge.begin(); it != edge.end(); ++it)
     {
-      auto jt = (*it).second.begin();
-      ++jt;
-      auto prev = jt;
-      for(; jt != (*it).second.end(); ++jt)
-      {
-        prev = jt;
-        --prev;
-        glm::vec2 p1 = (*prev) + (*it).first;
-        glm::vec2 p2 = (*jt) + (*it).first;
-        image.DrawLine(p1, p2, 0x00FF00FF);
-      }
-      prev = (*it).second.end();
-      --prev;
-      jt = (*it).second.begin();
-      glm::vec2 p1 = (*prev) + (*it).first;
-      glm::vec2 p2 = (*jt) + (*it).first;
+      glm::vec2 p1 = vertex[(*it).vertex1];
+      glm::vec2 p2 = vertex[(*it).vertex2];
       image.DrawLine(p1, p2, 0x00FF00FF);
     }
 
-    printf("%7gms Start saving\n", get_msec());
-    for(auto it = diagram.begin(); it != diagram.end();)
+    for(auto it = edge.begin(); it != edge.end(); ++it)
     {
-      auto range = diagram.equal_range((*it).first);
-      image.DrawPoint((*it).first, 0xFF0000FF);
-
-      it = range.second;
+      glm::vec2 p1 = points[(*it).site1];
+      glm::vec2 p2 = points[(*it).site2];
+      image.DrawPoint(p1, 0xFF0000FF);
+      image.DrawPoint(p2, 0xFF0000FF);
     }
 
+    printf("%7gms Start saving\n", get_msec());
 
     image.Save("img.png");
 
-    */
-
-  printf("%7gs End\n", get_msec());
+    printf("%7gs End\n", get_msec());
 
   //system("pause");
   return 0;
